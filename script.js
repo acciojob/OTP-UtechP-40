c240deeb56326ea368e11420d20271128ddee890
Pradeep Pandey <90238672+UtechP-40@users.noreply.github.com>
2024-09-26 04:11:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your JS code here. If required.
+let codes = document.querySelectorAll(".code")
+
+for(let i =0;i<codes.length-1;i++){
+    codes[i].addEventListener("input",()=>{
+        codes[i+1].focus()
+    })
+}
